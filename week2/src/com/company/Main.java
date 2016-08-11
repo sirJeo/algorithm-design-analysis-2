5bef79f75f8309c9441cbcf61fbe44e14b30d397
s.stupachenko <serhiy.stupachenko@gmail.com>
2016-08-11 18:18:16 +0300

    week2 done

--- week2/src/com/company/Main.java
+++ week2/src/com/company/Main.java
@@ -6,8 +6,7 @@ public class Main {
         Clustering1 o1 = new Clustering1("clustering1.txt");
         System.out.println("PA1: " + o1.calculate());
 
-//        Clustering2 o2 = new Clustering2("clustering2.txt");
-//        Clustering2 o2 = new Clustering2("test.txt");
-//        System.out.println("PA2: " + o2.calculate());
+        Clustering2 o2 = new Clustering2("clustering2.txt");
+        System.out.println("PA2: " + o2.calculate());
     }
 }
